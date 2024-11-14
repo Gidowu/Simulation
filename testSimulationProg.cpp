096442674c17aa9cf89010848c89e8403a4c3285
Grace Murphy <gracemurphy@Graces-MacBook-Air-3.local>
2024-11-13 21:28:47 -0500

    runSimulation

--- testSimulationProg.cpp
+++ testSimulationProg.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
-#include "simulation.h"
+#include "simulation.h"  // Ensure this header is included!
 #include "queueAsArray.h"
-#include "simulationImp.cpp"
+#include "simulationImp.cpp"  // The implementation for the simulation
 
 using namespace std;
 
@@ -46,37 +46,37 @@ void runSimulation()
     int servedCount = 0;
     int customersLeftInQueue = 0;
 
-    queueType<int> waitingQueue;
-
-    serverListType servers(numOfServers);
+    queueType<int> waitingQueue;  // Queue to hold customer IDs or waiting times
+    serverListType servers(numOfServers);  // Assuming this is defined in simulation.h
 
     for (clock = 1; clock <= simulationTime; clock++)
     {
-        servers.updateServers(cout);
+        servers.updateServers(cout);  // Update the servers (prints the status)
 
         if (!waitingQueue.isEmptyQueue())
         {
-            waitingQueue.incrementWaitingTimes();
+            waitingQueue.incrementWaitingTimes();  // Increment the waiting times of all customers
         }
 
+        // Every time between customer arrival
         if (clock % timeBetweenCustomerArrival == 0)
         {
-            customerCount++;
-            waitingQueue.addQueue(customerCount);
+            customerCount++;  // New customer arrives
+            waitingQueue.addQueue(customerCount);  // Add the new customer to the queue
         }
 
         int freeServerID = servers.getFreeServerID();
-        if (freeServerID != -1 && !waitingQueue.isEmptyQueue())
+        if (freeServerID != -1 && !waitingQueue.isEmptyQueue())  // Check if a server is free
         {
-            int customer = waitingQueue.front();
-            waitingQueue.deleteQueue();
-            servers.setServerBusy(freeServerID, transactionTime);
-            servedCount++;
+            int customer = waitingQueue.front();  // Get the customer at the front
+            waitingQueue.deleteQueue();  // Remove the customer from the queue
+            servers.setServerBusy(freeServerID, transactionTime);  // Assign server to the customer
+            servedCount++;  // Increment the served count
         }
     }
 
-    customersLeftInQueue = waitingQueue.getQueueCount();
-    int customersWithServers = servers.getNumberOfBusyServers();
+    customersLeftInQueue = waitingQueue.getQueueCount();  // Get remaining customers in the queue
+    int customersWithServers = servers.getNumberOfBusyServers();  // Get the number of busy servers
 
     cout << "Simulation results:" << endl;
     cout << "Total customers arrived: " << customerCount << endl;
